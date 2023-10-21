cf01efa816a2d5e4298a77753106965d26702f1f
Shan3774 <sifmek2@gmail.com>
2023-10-21 11:32:43 +0300

    feat: add blog preview

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {title:"Mario bought a car", author:"Balotelli", body:"What a car...", id:1},
+        {title:"Such a great brother - Luigi", author:"Mario", body:"Brotherhood...", id:2},
+        {title:"A name made popular", author:"Abebe", body:"Abebe beso bela... egr yblana", id:3}
+    ])
+
     return ( 
         <div className="home">
-            <h1>Home Page content</h1>
+            {blogs.map((blog) => {
+                return(
+                <div className="blogPreview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written By: {blog.author}</p>
+                </div>
+                )
+            })}
         </div>
      );
 }
